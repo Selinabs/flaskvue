762164c4615340378d3d91c98069ce227372f832
zixuan <2696055878@qq.com>
2019-12-05 22:53:22 +0800

    为vue框架添加页面

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+
+const routerOptions = [
+  { path: '/', component: 'Home' },
+  { path: '/about', component: 'About' }
+]
+const route = routerOptions.map(route => {
+  // let router;
+  return {
+    // eslint-disable-next-line no-undef
+    ...route,
+    component: () => import(`@/components/${route.component}.vue`)
+  }
+})
 
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
+  route,
+  mode: 'history'
+  // routes: [
+  //   {
+  //     path: '/',
+  //     name: 'HelloWorld',
+  //     component: HelloWorld
+  //   }
+  // ]
 })

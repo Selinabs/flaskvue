3e10c2aede678fab31a7e506ca2875df230fe2a3
zixuan <2696055878@qq.com>
2019-12-16 22:22:36 +0800

    新建flask项目

---
+++ app.py
@@ -0,0 +1,19 @@
+# _*_ coding : UTF-8 _*_
+# 开发人员："夏沫丶"
+# 开发时间： 2019/12/16 21:35
+# 文件名称： app.py
+# 开发工具： PyCharm
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/greek")
+@app.route('/greek/<request>')
+def say_hello(request="cc"):
+    a = 1
+    b = 2
+    return f"{a + b} hello {request}"
+
+
+if __name__ == "__main__":
+    app=app.run(host='localhost',debug="Ture",port=80)
